2bee3d59fbdd63f3892f6c294fda70fc0751c3af
bezjac <mybj20@gmail.com>
2021-10-06 17:09:25 +0300

    Initial code + Welcome functions

---
+++ dotNet5782_3236_7943/Targil0/Program3236.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome3236();
+            Welcome7943();
+            Console.ReadKey();
+        }
+        static partial void Welcome7943();
+        private static void Welcome3236()
+        {
+            Console.Write("Enter your name: ");
+            String userName = Console.ReadLine();
+            Console.WriteLine(userName + ", welcome to my first console application");
+        }
+    }
+}
